237353ba610e0b97bf9a6002758487c6e0fb899c
npal <mr.npal.dev@gmail.com>
2025-05-20 23:47:44 +0700

    move group chat

--- lib/views/splash.dart
+++ lib/views/splash.dart
@@ -1,4 +1,4 @@
-import 'package:alumni_busfa/views/user_home.dart';
+import 'package:alumni_busfa/views/home_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:alumni_busfa/views/welcome_page.dart';
